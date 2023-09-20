0633ee3f505c252aa5ca28e189205111c493e0d5
clauber-DEV <clauber.porto@hotmail.com>
2023-09-20 11:28:11 -0300

    calculando IMC

--- 001/index.js
+++ 001/index.js
@@ -4,4 +4,8 @@
  * ../ == voltar uma pasta
  * control s == salvar arquivo
  * clear == limpar terminal
- */
+ * 
+ * Falsy == false, 0, "", NaN, undefined e null são considerados falso
+ * Truthy == Todos os outros diferentes de Falsy
+ * obs: o comando to fixed não só arredonda o número como tambem converte para string.
+*/
