cb2085ce899d349afa3d55e4a88a6f1c7f754840
clauber-DEV <clauber.porto@hotmail.com>
2023-09-27 21:26:16 -0300

    Create index.js

---
+++ anotacoes/Closures/index.js
@@ -0,0 +1,44 @@
+/*
+ *  Uma Closure ocorre normalmente quando uma 
+ * função é declarada dentro do corpo da outra, e a 
+ * função interior refencia variaveis locais da função exterior.
+ */
+
+// ex utilizando String:
+function QualSeuNome(pnome){
+    const msg = `O seu nome é: `
+    function nometodo (){
+        return `${msg} ${pnome}`
+    }
+    return nometodo()
+}
+
+console.log(QualSeuNome("Clauber"))
+
+// ex utilizandoo Numbers
+
+function Calculadora(num1,num2){
+    const msg = `Resultado`
+
+    const soma = () => {
+        return `${msg} ${num1 + num2}`
+    }
+    const subtracao = () => {
+        return `${msg} ${num1 - num2}`
+    }
+    const multplicasao = () => {
+        return `${msg} ${num1 * num2}`
+    }
+    const divisao = () => {
+        return `${msg} ${num1 / num2}`
+    }
+
+    return {
+        soma: soma(),
+        subtracao: subtracao(),
+        multplicasao: multplicasao(),
+        divisao: divisao()
+    }
+}
+
+console.log(Calculadora(12, 6))
