046e8357fdc5d7694b86e7845a8bdbfd0899b3cb
clauber-DEV <clauber.porto@hotmail.com>
2023-10-10 10:40:26 -0300

    Update index.js

--- anotacoes/objetos/acessando-objetos/index.js
+++ anotacoes/objetos/acessando-objetos/index.js
@@ -1,6 +1,11 @@
-const Tenis ={
+const Tenis = {
     tamanho: 45,
     marca: "MacBoot",
 };
-
-console.log(Tenis.marca);
+// O elemnto "." é utiizado para acessar as propriedades interna de um outro elemento.
+console.log(Tenis.marca); //está acessando a propriedade marca do objeto Tenis
+console.log(Tenis.tamanho); //está acessando a propriedade marca do objeto Tenis
+console.log(typeof Tenis.tamanho);
+console.log(Tenis.tamanho + 5);
+console.log("Marca" + Tenis.marca + "è muito boa!"); 
+console.log(`Marca ${Tenis.marca} é muito boa!`)
