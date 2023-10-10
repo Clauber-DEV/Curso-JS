452170d9e13904786f69f18f9ef32e12f09c8b3d
clauber-DEV <clauber.porto@hotmail.com>
2023-10-10 11:40:07 -0300

    Create index.js

---
+++ anotacoes/objetos/Destruturing-objetos/index.js
@@ -0,0 +1,27 @@
+const Tenis ={
+    tamanho: 45,
+   // marcas: [{nome: "MacBoot"}, {nome: "Ombongo"}],
+    estoque: true,
+    secret: 123456,
+    n: "MG",
+    link: {a: "prop a", b: {c:"prop c"}},
+};
+// Destructuring utilizado na linha 7
+const {tamanho, estoque, marcas = "Não possui Marcas"}= Tenis; /* nesse treixo: "marcas = "Não possui Marcas"" caso não axista propriedade "marcas* será retornado a freze cmo de fosse uma condição if conpactada.*/
+
+console.log(tamanho, estoque, marcas);
+
+// Destructuring utilizado na linha 14
+const{ secret: numeroqualquer, n: tamanhoextra} = Tenis; // substituição do nome das propriedades
+console.log(numeroqualquer);
+console.log(tamanhoextra);
+
+
+const{
+    link:{
+        a,
+        b:{c},
+    }
+} = Tenis
+
+console.log(c);
