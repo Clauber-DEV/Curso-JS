c2da16c887643e7e85c5f6919c025114fd153957
clauber-DEV <clauber.porto@hotmail.com>
2023-10-20 09:04:48 -0300

    Update index.js

--- anotacoes/objetos/Mesclando-e-clonando-objeto/index.js
+++ anotacoes/objetos/Mesclando-e-clonando-objeto/index.js
@@ -9,11 +9,11 @@ let clone1 = Tenis; // clone simples
 let mesclar1 = Object.assign(Tenis, link);// mesclando dois objetos com comando assign
 let mesclar2 = {...Tenis, ...link}; // forma compacta de mesclagem
 
-console.log("clone1"+clone1);
+console.log("clone1",clone1);
 
 clone1.estoque = false; // modificação no elemento estoque do objeto Tenis
 mesclar1.link.a = "ABC";// modificação no elemento "a" do objeto link
-
-console.log("mesclar 1"+mesclar1);
-console.log("mesclar 2"+mesclar2);
-console.log("objeto Tenis"+Tenis);
+/**este forma de modificação pode gerar alguns problemas como referencias pegas erradas como é mostrado no terminal. */
+console.log("mesclar1",mesclar1);
+console.log("mesclar2",mesclar2);
+console.log("Tenis",Tenis);
