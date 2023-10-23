9c323d611b23228bcb44d58f54fc2affa79b17e4
clauber-DEV <clauber.porto@hotmail.com>
2023-10-23 10:19:11 -0300

    vetor

---
+++ anotacoes/Arrays/index.js
@@ -0,0 +1,7 @@
+/** Array ou vetores em JS são variaveis capazes de armasenar varios dados ou elementos diferentes */
+
+const vetor = [1,2,3,4, true, "Clauber", {tipo: "objetos"}]
+
+console.table(vetor); // exibir em forma de tabela
+console.log(vetor); // exibir em forma de linha
+console.log(vetor.length); // exibir a uantidade de indices
