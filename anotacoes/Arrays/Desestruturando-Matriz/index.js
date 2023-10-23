7b584347abea7a643d8b6e0e50607585bdd857eb
clauber-DEV <clauber.porto@hotmail.com>
2023-10-23 11:03:38 -0300

    desestruturando matriz

---
+++ anotacoes/Arrays/Desestruturando-Matriz/index.js
@@ -0,0 +1,11 @@
+const [a,b,c] = ["Arroz","Feijão","Carne"];
+const almoco = ["Macarrão","Salada","Frango"];/** nesse exemplo a matriz almoco
+ o indice [0] recebe string "Macarrão"
+ o indice [1] recebe string "Salada"
+ o indice [2] recebe string "Frango"*/
+
+console.log(a);// recebe string "Arroz"
+console.log(b);// recebe string "Feijão"
+console.log(c);// recebe string "Carne"
+
+console.log(almoco[0],almoco[1],almoco[2]);
