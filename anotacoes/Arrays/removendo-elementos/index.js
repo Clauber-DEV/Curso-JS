c61895201cabf5b5b0f25e80be1c83e8a1f00dc5
clauber-DEV <clauber.porto@hotmail.com>
2023-10-25 10:31:36 -0300

    removendo elementos

---
+++ anotacoes/Arrays/removendo-elementos/index.js
@@ -0,0 +1,12 @@
+/** Comando "Shift" remove o primeiro item do Array e retorna esse elemento.
+ *  Comando "pop" remove o ultimo elemento de um Array e retorna esse elemento.
+*/
+
+let vetor = [1, 2, 3, 4, 5,];
+
+console.table(vetor);
+
+console.log("shift =>", vetor.shift());
+console.log("pop =>", vetor.pop());
+
+console.table(vetor);
